a0074228686731b54d43861303c0f3be85299758
Yuta Hayashi <yuta.hayashi.soccer@gmail.com>
2020-11-03 21:59:57 +0900

    Modified bubble_sort.py

--- sort_algorithm/bubble_sort.py
+++ sort_algorithm/bubble_sort.py
@@ -1,9 +1,9 @@
-# try to implement this on your own
+# # try to implement this on your own
 
 from typing import List
 
 def bubble_sort(numbers: List[int]) -> List[int]:
-    len_numbers = len(numbers )
+    len_numbers = len(numbers)
     for i in range(len_numbers):
         for j in range(len_numbers - 1 - i):
             if numbers[j] > numbers[j+1]:
@@ -15,3 +15,13 @@ def bubble_sort(numbers: List[int]) -> List[int]:
 if __name__ == "__main__":
     nums = [2,5,1,8,7,3]
     print(bubble_sort(nums))
+
+def bubble(arr, n):
+    for i in range(0, n-1):
+        for j in range(n-1, i, -1):
+            if arr[j] < arr[j-1]:
+                arr[j], arr[j-1] = arr[j-1], arr[j]
+
+arr = [5,9,2,1,7,3,4,6,8,0]
+bubble(arr, len(arr))
+print(arr)
