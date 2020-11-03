5746e801671148c3568235d658f8456fa398d7f1
Yuta Hayashi <yuta.hayashi.soccer@gmail.com>
2020-11-03 21:20:58 +0900

    Created insertion_sort.py

---
+++ sort_algorithm/insertion_sort.py
@@ -0,0 +1,31 @@
+# from typing import List
+#
+# def insertion_sort(numbers: List[int]) -> List[int]:
+#     len_numbers = len(numbers)
+#     for i in range(1, len_numbers):
+#         temp = numbers[i]
+#         j = i - 1
+#         while j >= 0 and numbers[j]>temp:
+#             numbers[j+1] = numbers[j]
+#             j -= 1
+#         numbers[j+1] = temp
+#
+#     return numbers
+#
+# if __name__ == '__main__':
+#     nums = [1,7,3,2,8,5]
+#     print(insertion_sort(nums))
+
+def insertion(arr, n):
+    for i in range(1, n):
+        val = arr[i]
+
+        j = i - 1
+        while j >= 0 and arr[j] > val:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = val
+
+arr = [5,9,2,1,7,3,4,6,8,0]
+insertion(arr, len(arr))
+print(arr)
