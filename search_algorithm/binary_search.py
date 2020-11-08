580d0857c8c414d96e6b29d26a08e8548b39696f
Yuta Hayashi <yuta.hayashi.soccer@gmail.com>
2020-11-08 21:17:37 +0900

    binary search

---
+++ search_algorithm/binary_search.py
@@ -0,0 +1,21 @@
+
+my_list = [1,5,6,8,2,3,9]
+
+
+def binary_search(list, item):
+    low = 0
+    high = len(list) - 1
+
+    while low <= high:
+        mid = (low + high) //2
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid -1
+        else:
+            low = mid + 1
+
+    return None
+
+print (binary_search(my_list, 8))
